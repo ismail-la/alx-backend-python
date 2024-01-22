ccc0871dd328206ca56bc24df4eaa9a93f8b7857
ismail-la <lahbariismail@gmail.com>
2024-01-22 22:00:08 +0100

    Solutions to tasks on 0x02. Python - Async Comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -9,10 +9,10 @@ over async_generator, then return the 10 random numbers.
 
 
 from typing import List
-
-async_generator = __import__("0-async_generator").async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
     """Return the 10 random numbers"""
-    return [random async for random in async_generator()]
+    results = [i async for i in async_generator()]
+    return results
