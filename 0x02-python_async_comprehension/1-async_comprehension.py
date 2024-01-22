1eccddadad9d01046cdd48679bb5b7dc9f5cd8a2
ismail-la <lahbariismail@gmail.com>
2024-01-22 22:02:55 +0100

    Solutions to tasks on 0x02. Python - Async Comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Module documentation 
+""" Module documentation
 Import async_generator from the previous task and then write a coroutine
 called async_comprehension that takes no arguments.
 
@@ -14,5 +14,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """Return the 10 random numbers"""
-    results = [i async for i in async_generator()]
-    return results
+    res = [i async for i in async_generator()]
+    return res
